8d0e8665f24ae9d4b6014e7c30dfde5c9cf07d1c
xiaoyanu <61397705+xiaoyanu@users.noreply.github.com>
2023-01-15 10:30:48 +0800

    禁用邮件推送

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ LastEditTime: 2021-07-22 17:35:09
 Description: 主运行模块
 '''
 from types import MemberDescriptorType
-from email_service import EmailService
+#from email_service import EmailService 禁用邮件推送
 from api_web import SkyTask
 from log import log
 import sqlite3
@@ -39,7 +39,7 @@ def writeSQL(title, url, html) -> bool:
 
     except sqlite3.IntegrityError:
         # log.logger.warning(f"{url}数据已存在")
-        return True
+        return False
     except Exception as e:
         log.logger.critical(f"{url}其他错误: {e}")
         return False
@@ -52,10 +52,10 @@ def main():
     '''主运行函数'''
     log.logger.info("爬取开始ing......")
     # 各种实例化
-    mail = EmailService()
+#    mail = EmailService() 禁用邮件推送
     spider = SkyTask()
     # 获取链接列表,循环直至成功,应对复杂的网络环境
-    for i in range(3):
+    for i in range(11):
         urls = spider.getIndex()
         if urls:
             break
@@ -67,7 +67,7 @@ def main():
     for url in urls:
         # 解析单个文章
         # 重试
-        for i in range(3):
+        for i in range(11):
             title, html = spider.parse(url)
             if html:
                 break
@@ -80,7 +80,7 @@ def main():
             count += 1
             # 写入文件
             md_path, html_content = spider.writeDoc(md, html, title)
-            mail.send_emails(html_content, fileList=[md_path, "run.log"]) # 支持多邮箱发送.
+ #           mail.send_emails(html_content, fileList=[md_path, "run.log"]) # 支持多邮箱发送. 禁用邮件推送
 
     log.logger.info(f"处理完成! 新数据条数:{count}")
 
